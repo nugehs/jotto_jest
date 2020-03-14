dfd2eed2c98bb1bdbad3b947da670ad61d06426c
Segun Olumbe <david.olumbe@gmail.com>
2020-03-14 14:21:59 +0000

    setuptest config

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import {
     findByTestAttr,
     checkPropsType
 } from '../test/testUtil';
 
 import Congrats from './Congrats';
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 const defaultProps = { success: false };
 
