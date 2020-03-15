c9e58554f15a73b219074535413bb18d14732db6
Segun Olumbe <david.olumbe@gmail.com>
2020-03-15 22:45:41 +0000

    setup guessed word

---
+++ src/integration.test.js
@@ -0,0 +1,23 @@
+import { storeFactory} from "../test/testUtil";
+import { guessWord } from './actions';
+
+describe('guessWords action dispatcher', () => {
+    describe('no guessed words', () => {
+        test('update state correctly for unsuccessfully guess', () => {
+
+        });
+
+        test('update state correctly for successfully guess', () => {
+
+        });
+    });
+
+    describe('some guessed words', () => {
+        test('update state correctly for unsuccessfully guess', () => {
+
+        });
+        test('update state correctly for successfully guess', () => {
+
+        });
+    });
+});
