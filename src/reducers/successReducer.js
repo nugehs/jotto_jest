01a77d657d429b64fcf8d7c8ef8636683a6f4fde
Segun Olumbe <david.olumbe@gmail.com>
2020-03-15 20:42:13 +0000

    setup redux

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function successReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} action - action to be reduced.
+ * @param {boolean} state - new success state.
+ */
+export default (state, action) => {
+    return null;
+}
